8f7fc837141b253915e0beee4fc420da64227117
Yavor Petrov <petrovkunev@gmail.com>
2025-02-04 01:52:00 +0200

    Update Form1.Designer.cs

--- WinFormsApps_Exercise/Calculator/Form1.Designer.cs
+++ WinFormsApps_Exercise/Calculator/Form1.Designer.cs
@@ -35,7 +35,7 @@
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(609, 396);
+            ClientSize = new Size(557, 396);
             FormBorderStyle = FormBorderStyle.FixedDialog;
             Icon = (Icon)resources.GetObject("$this.Icon");
             MaximizeBox = false;
