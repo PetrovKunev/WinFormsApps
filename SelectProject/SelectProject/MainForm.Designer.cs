1e11889e5d8091a999d1fea5258abf6ca73b18ff
Yavor Petrov <147103195+PetrovKunev@users.noreply.github.com>
2025-02-05 20:17:20 +0200

    Icon

--- SelectProject/SelectProject/MainForm.Designer.cs
+++ SelectProject/SelectProject/MainForm.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
             btnSelect = new Button();
             lblSelectedProject = new Label();
             lstAvailableProjects = new ListBox();
@@ -39,11 +40,13 @@
             // btnSelect
             // 
             btnSelect.BackColor = Color.FromArgb(255, 128, 128);
+            btnSelect.FlatStyle = FlatStyle.Popup;
             btnSelect.Font = new Font("Calibri", 28F);
             btnSelect.ForeColor = Color.LawnGreen;
-            btnSelect.Location = new Point(482, 46);
+            btnSelect.Location = new Point(895, 98);
+            btnSelect.Margin = new Padding(6);
             btnSelect.Name = "btnSelect";
-            btnSelect.Size = new Size(290, 109);
+            btnSelect.Size = new Size(539, 233);
             btnSelect.TabIndex = 0;
             btnSelect.Text = "Избери проект";
             btnSelect.UseVisualStyleBackColor = false;
@@ -54,30 +57,35 @@
             lblSelectedProject.AutoSize = true;
             lblSelectedProject.Font = new Font("Calibri", 26.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
             lblSelectedProject.ForeColor = Color.GhostWhite;
-            lblSelectedProject.Location = new Point(435, 257);
+            lblSelectedProject.Location = new Point(808, 548);
+            lblSelectedProject.Margin = new Padding(6, 0, 6, 0);
             lblSelectedProject.Name = "lblSelectedProject";
-            lblSelectedProject.Size = new Size(392, 42);
+            lblSelectedProject.Size = new Size(807, 86);
             lblSelectedProject.TabIndex = 1;
             lblSelectedProject.Text = "______________________";
             // 
             // lstAvailableProjects
             // 
+            lstAvailableProjects.BackColor = SystemColors.Info;
             lstAvailableProjects.Font = new Font("Calibri", 20.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
             lstAvailableProjects.FormattingEnabled = true;
-            lstAvailableProjects.ItemHeight = 33;
-            lstAvailableProjects.Location = new Point(40, 46);
+            lstAvailableProjects.ItemHeight = 66;
+            lstAvailableProjects.Location = new Point(74, 98);
+            lstAvailableProjects.Margin = new Padding(6);
             lstAvailableProjects.Name = "lstAvailableProjects";
-            lstAvailableProjects.Size = new Size(325, 433);
+            lstAvailableProjects.Size = new Size(600, 862);
             lstAvailableProjects.TabIndex = 2;
             // 
             // lstSelectedProjects
             // 
+            lstSelectedProjects.BackColor = SystemColors.Info;
             lstSelectedProjects.Font = new Font("Calibri", 20.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
             lstSelectedProjects.FormattingEnabled = true;
-            lstSelectedProjects.ItemHeight = 33;
-            lstSelectedProjects.Location = new Point(883, 46);
+            lstSelectedProjects.ItemHeight = 66;
+            lstSelectedProjects.Location = new Point(1640, 98);
+            lstSelectedProjects.Margin = new Padding(6);
             lstSelectedProjects.Name = "lstSelectedProjects";
-            lstSelectedProjects.Size = new Size(325, 433);
+            lstSelectedProjects.Size = new Size(600, 862);
             lstSelectedProjects.TabIndex = 3;
             // 
             // label1
@@ -85,9 +93,10 @@
             label1.AutoSize = true;
             label1.Font = new Font("Calibri", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
             label1.ForeColor = Color.WhiteSmoke;
-            label1.Location = new Point(40, 20);
+            label1.Location = new Point(74, 43);
+            label1.Margin = new Padding(6, 0, 6, 0);
             label1.Name = "label1";
-            label1.Size = new Size(149, 23);
+            label1.Size = new Size(303, 46);
             label1.TabIndex = 4;
             label1.Text = "Налични проекти";
             // 
@@ -96,24 +105,27 @@
             label2.AutoSize = true;
             label2.Font = new Font("Calibri", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
             label2.ForeColor = Color.WhiteSmoke;
-            label2.Location = new Point(883, 20);
+            label2.Location = new Point(1640, 43);
+            label2.Margin = new Padding(6, 0, 6, 0);
             label2.Name = "label2";
-            label2.Size = new Size(148, 23);
+            label2.Size = new Size(301, 46);
             label2.TabIndex = 5;
             label2.Text = "Избрани проекти";
             // 
             // MainForm
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(13F, 32F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = SystemColors.ActiveCaption;
-            ClientSize = new Size(1243, 599);
+            ClientSize = new Size(2308, 1278);
             Controls.Add(label2);
             Controls.Add(label1);
             Controls.Add(lstSelectedProjects);
             Controls.Add(lstAvailableProjects);
             Controls.Add(lblSelectedProject);
             Controls.Add(btnSelect);
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            Margin = new Padding(6);
             Name = "MainForm";
             Text = "Select Projects";
             ResumeLayout(false);
