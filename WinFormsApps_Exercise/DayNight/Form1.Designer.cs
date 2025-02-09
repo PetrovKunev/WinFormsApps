150ecf1071cf2b1fc043f92573f65f5d46f97b1d
Yavor Petrov <petrovkunev@gmail.com>
2025-02-10 01:17:34 +0200

    Day and Night

---
+++ WinFormsApps_Exercise/DayNight/Form1.Designer.cs
@@ -0,0 +1,79 @@
+﻿namespace DayNight
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            radioBtnDay = new RadioButton();
+            radioBtnNight = new RadioButton();
+            SuspendLayout();
+            // 
+            // radioBtnDay
+            // 
+            radioBtnDay.AutoSize = true;
+            radioBtnDay.Location = new Point(329, 121);
+            radioBtnDay.Name = "radioBtnDay";
+            radioBtnDay.Size = new Size(45, 19);
+            radioBtnDay.TabIndex = 0;
+            radioBtnDay.TabStop = true;
+            radioBtnDay.Text = "Day";
+            radioBtnDay.UseVisualStyleBackColor = true;
+            radioBtnDay.CheckedChanged += radioButtonDay_CheckedChanged;
+            // 
+            // radioBtnNight
+            // 
+            radioBtnNight.AutoSize = true;
+            radioBtnNight.Location = new Point(329, 179);
+            radioBtnNight.Name = "radioBtnNight";
+            radioBtnNight.Size = new Size(55, 19);
+            radioBtnNight.TabIndex = 1;
+            radioBtnNight.TabStop = true;
+            radioBtnNight.Text = "Night";
+            radioBtnNight.UseVisualStyleBackColor = true;
+            radioBtnNight.CheckedChanged += radioButtonNight_CheckedChanged;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.White;
+            ClientSize = new Size(800, 450);
+            Controls.Add(radioBtnNight);
+            Controls.Add(radioBtnDay);
+            Name = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Day and Night";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private RadioButton radioBtnDay;
+        private RadioButton radioBtnNight;
+    }
+}
---
+++ WinFormsApps_Exercise/DayNight/Form1.cs
@@ -0,0 +1,76 @@
+﻿namespace DayNight
+{
+    public partial class Form1 : Form
+    {
+        //bool isDay;
+        //public Form1()
+        //{
+        //    InitializeComponent();
+        //}
+
+        //private void Checked_Status(object sender, EventArgs e)
+        //{
+        //    isDay = radioBtnDay.Checked;
+
+        //    if (isDay)
+        //    {
+        //        this.BackColor = Color.White;
+        //        this.ForeColor = Color.Black;
+        //    }
+        //    else
+        //    {
+        //        this.BackColor = Color.Black;
+        //        this.ForeColor = Color.White;
+        //    }
+        //}
+
+        
+          // Деклариране на булевата променлива isDay
+        private bool isDay = true; // по подразбиране избираме дневен режим
+
+        public Form1()
+        {
+            InitializeComponent();
+            // Задаване на началното състояние: избран е режим "Ден"
+            radioBtnDay.Checked = true;
+            UpdateTheme();
+        }
+
+        // Обработчик за събитието CheckedChanged на radioButtonDay
+        private void radioButtonDay_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioBtnDay.Checked)
+            {
+                isDay = true;
+                UpdateTheme();
+            }
+        }
+
+        // Обработчик за събитието CheckedChanged на radioButtonNight
+        private void radioButtonNight_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioBtnNight.Checked)
+            {
+                isDay = false;
+                UpdateTheme();
+            }
+        }
+
+        // Метод, който обновява външния вид на формата според избрания режим
+        private void UpdateTheme()
+        {
+            if (isDay)
+            {
+                // За дневен режим: светъл фон
+                this.BackColor = Color.White;
+                this.ForeColor = Color.Black;
+            }
+            else
+            {
+                // За нощен режим: тъмен фон
+                this.BackColor = Color.FromArgb(30, 30, 30);
+                this.ForeColor = Color.White;
+            }
+        }
+    }
+}
