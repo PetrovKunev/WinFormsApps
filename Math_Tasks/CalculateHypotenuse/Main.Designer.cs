18d03f8635264adc65fda589c3be3cf6a7fc2d86
Yavor Petrov <147103195+PetrovKunev@users.noreply.github.com>
2025-03-04 20:52:06 +0200

    CalculateHypotenuse Update

--- Math_Tasks/CalculateHypotenuse/Main.Designer.cs
+++ Math_Tasks/CalculateHypotenuse/Main.Designer.cs
@@ -28,19 +28,122 @@
         /// </summary>
         private void InitializeComponent()
         {
+            txtCadetA = new TextBox();
+            txtCadetB = new TextBox();
+            label1 = new Label();
+            label2 = new Label();
+            btnCalculate = new Button();
+            lblResult = new Label();
+            pictureBox1 = new PictureBox();
+            btnClear = new Button();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             SuspendLayout();
             // 
-            // Form1
+            // txtCadetA
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            txtCadetA.Location = new Point(106, 160);
+            txtCadetA.Name = "txtCadetA";
+            txtCadetA.Size = new Size(348, 39);
+            txtCadetA.TabIndex = 0;
+            // 
+            // txtCadetB
+            // 
+            txtCadetB.Location = new Point(106, 354);
+            txtCadetB.Name = "txtCadetB";
+            txtCadetB.Size = new Size(339, 39);
+            txtCadetB.TabIndex = 1;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(106, 80);
+            label1.Name = "label1";
+            label1.Size = new Size(165, 32);
+            label1.TabIndex = 2;
+            label1.Text = "Insert cadet A:";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(106, 264);
+            label2.Name = "label2";
+            label2.Size = new Size(159, 32);
+            label2.TabIndex = 3;
+            label2.Text = "Insert cadet B";
+            // 
+            // btnCalculate
+            // 
+            btnCalculate.Font = new Font("Calibri", 18F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            btnCalculate.Location = new Point(106, 645);
+            btnCalculate.Name = "btnCalculate";
+            btnCalculate.Size = new Size(339, 84);
+            btnCalculate.TabIndex = 4;
+            btnCalculate.Text = "Calculate";
+            btnCalculate.UseVisualStyleBackColor = true;
+            btnCalculate.Click += btnCalculate_Click;
+            // 
+            // lblResult
+            // 
+            lblResult.AutoSize = true;
+            lblResult.Font = new Font("Calibri", 16.125F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            lblResult.Location = new Point(925, 676);
+            lblResult.Name = "lblResult";
+            lblResult.Size = new Size(212, 53);
+            lblResult.TabIndex = 5;
+            lblResult.Text = "_________";
+            lblResult.TextAlign = ContentAlignment.TopCenter;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BackgroundImage = Properties.Resources.Pythagorean_Theorem;
+            pictureBox1.BackgroundImageLayout = ImageLayout.Stretch;
+            pictureBox1.Location = new Point(760, 80);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(566, 406);
+            pictureBox1.TabIndex = 6;
+            pictureBox1.TabStop = false;
+            // 
+            // btnClear
+            // 
+            btnClear.Font = new Font("Calibri", 18F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            btnClear.Location = new Point(512, 645);
+            btnClear.Name = "btnClear";
+            btnClear.Size = new Size(186, 84);
+            btnClear.TabIndex = 7;
+            btnClear.Text = "Clear";
+            btnClear.UseVisualStyleBackColor = true;
+            btnClear.Click += btnClear_Click;
+            // 
+            // Main
+            // 
+            AutoScaleDimensions = new SizeF(13F, 32F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
-            Name = "Calculate hypotenuse";
+            ClientSize = new Size(1486, 833);
+            Controls.Add(btnClear);
+            Controls.Add(pictureBox1);
+            Controls.Add(lblResult);
+            Controls.Add(btnCalculate);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(txtCadetB);
+            Controls.Add(txtCadetA);
+            Margin = new Padding(6);
+            Name = "Main";
             Text = "Pythagorean theorem";
-            
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private TextBox txtCadetA;
+        private TextBox txtCadetB;
+        private Label label1;
+        private Label label2;
+        private Button btnCalculate;
+        private Label lblResult;
+        private PictureBox pictureBox1;
+        private Button btnClear;
     }
 }
--- Math_Tasks/CalculateHypotenuse/Main.cs
+++ Math_Tasks/CalculateHypotenuse/Main.cs
@@ -2,10 +2,36 @@ namespace CalculateHypotenuse
 {
     public partial class Main : Form
     {
+        double cadetA;
+        double cadetB;
+        double hypotenuseC;
         public Main()
         {
             InitializeComponent();
         }
 
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            cadetA = double.Parse(txtCadetA.Text);
+            cadetB = double.Parse(txtCadetB.Text);
+
+            if (cadetA <= 0 || cadetB <= 0)
+            {
+                MessageBox.Show("Both cadetA and cadetB must be greater than zero.", "Input Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+                
+            }
+
+            hypotenuseC = Math.Sqrt(Math.Pow(cadetA, 2) + Math.Pow(cadetB, 2));
+
+            lblResult.Text = $"Hypotenuse C = {hypotenuseC:F2}";
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtCadetA.Text = "";
+            txtCadetB.Text = "";
+            lblResult.Text = "_________";
+        }
     }
 }
